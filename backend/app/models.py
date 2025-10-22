fc8615ce01433a58cd1cd03a77c1a37771dc283f
Flavio Bueno <flavio.bueno86@gmail.com>
2025-10-22 08:11:12 -0300

    feat: modelos SQLModel de Servico e Profissional

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlmodel import SQLModel, Field
+from sqlmodel import Relationship, SQLModel, Field
 from datetime import datetime
 
 class User(SQLModel, table=True):
@@ -26,3 +26,25 @@ class Agendamento(SQLModel, table=True):
     servico: str
     observacoes: Optional[str] = None
     created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+
+
+class Servico(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
+    descricao: Optional[str] = None
+    duracao_minutos: int
+    preco: float
+    created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+
+class ProfissionalServico(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    profissional_id: int = Field(foreign_key="profissional.id", primary_key=True)
+    servico_id: int = Field(foreign_key="servico.id", primary_key=True)
+    created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+
+class Profissional(SQLModel, table=True):
+    id: int | None
+    nome: str | None
+    email: str | None
+    ativo : bool = Field(default=True)
+    servicos: list["Servico"] = Relationship(back_populates="profissionais", link_model=ProfissionalServico)
